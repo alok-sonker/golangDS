7005c3d7e5411f7c1c9b67fb69b1a0448b285ed2
alok-sonker <sonker.alok7827@gmail.com>
2023-09-24 19:01:11 +0530

    hgdbf

--- main.go
+++ main.go
@@ -4,14 +4,11 @@
 
 package main
 
-import "fmt"
-
 func main() {
 	list := InitList()
 	list.Append(1, 0, 3, 5, 7)
 	list.Traverse()
 	list.Append(9, 10, 13, 15, 17)
-	fmt.Println("Testing mergeconflict")
 	list.Traverse()
 	list.Prepend(99, 100)
 	list.Traverse()
